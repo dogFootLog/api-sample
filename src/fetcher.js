383104a77c28c4b0dfd5cdd186ff83d96ba1ae30
dogFootLog <97735017+dogFootLog@users.noreply.github.com>
2022-08-25 18:32:28 +0900

    Update fetcher.js

--- src/fetcher.js
+++ src/fetcher.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 axios.defaults.baseURL =
-  'https://1b228c5f-b9b2-4ed8-96ca-8c0bfded127d.mock.pstmn.io';
+  'https://www.naver.com'; // URL 주소
 
 const fetcher = async (method, url, ...rest) => {
   const res = await axios[method](url, ...rest);
